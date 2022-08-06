7015550d1f20e5e03438f719ddb5d990759bfdb8
Everest22 <himalaypatel37@gmail.com>
2022-08-06 11:03:36 +0530

    final
    after successful code

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'model/transaction.dart';
+import 'package:intl/intl.dart';
+
+
 
 void main() {
   runApp(const MyApp());
@@ -16,23 +20,86 @@ class MyApp extends StatelessWidget {
 
         primarySwatch: Colors.blue,
       ),
-      home: const LoginPage(),
+      home: const MyHomePage(),
     );
   }
 }
 
-class LoginPage extends StatefulWidget {
-  const LoginPage({Key? key}) : super(key: key);
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
-  State<LoginPage> createState() => _LoginPageState();
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _LoginPageState extends State<LoginPage> {
+class _MyHomePageState extends State<MyHomePage> {
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
+
+  _addNewTransaction(String txTtile, double txAmount){
+    final newTx = Transaction(
+        id: 'Hs',
+        title: txTtile,
+        amount: txAmount,
+        date: DateTime.now(),
+    );
+    setState(() {
+      transactions.add(newTx);
+    });
+  }
+
+  List<Transaction> transactions = [
+    Transaction(id: 't1', title: 'books', amount: 255, date: DateTime.now()),
+    Transaction(id: 't2', title: 'nasta', amount: 100, date: DateTime.now()),
+    Transaction(id: 't3', title: 'park', amount: 255, date: DateTime.now()),
+
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Hello'),
+      appBar: AppBar(
+        title: Text('My Expense'),
+      ),
+       body: Column(
+         children: [
+           Card(
+             child: Column(
+               children: [
+                 TextField(
+                   controller: titleController,
+                   decoration: InputDecoration(
+                     hintText: 'Title',
+                   ),
+                 ),
+                 TextField(
+                   controller: amountController,
+                   decoration: InputDecoration(
+                     labelText: 'Amount',
+                   ),
+                 ),
+                 ElevatedButton(onPressed: () {
+                   _addNewTransaction(titleController.text, double.parse(amountController.text));
+                 }, child: Text('Submit')),
+               ],
+             ),
+           ),
+           Expanded(
+            child: ListView.builder(
+                itemCount: transactions.length,
+                itemBuilder: (BuildContext context, int index){
+                  return Card(
+                  elevation: 5,
+                  child: ListTile(
+                      title: Text('${transactions[index].title}'),
+                      subtitle: Text('${DateFormat('yyyy-MM-dd').format(transactions[index].date)}'),
+                      trailing: Text('${transactions[index].amount}'),
+                  ),
+                  );
+                  }
+            ),
+            ),
+        ],
+       ),
     );
   }
 }
